3dcb0bd4e933f2e2de4093801b7a9dc4f02e8f8f
Drushti Chepte <cheptedrushti@gmail.com>
2025-09-21 14:29:34 +0530

    reponsiveness fix and vercel file add

--- src/components/EmotionDisplay.jsx
+++ src/components/EmotionDisplay.jsx
@@ -1,7 +1,7 @@
 const EmotionDisplay = ({ emotion }) => {
   return (
     <div className="mt-6 font-semibold flex flex-col justify-center items-center gap-5 text-white text-nowrap">
-      <div className="text-2xl md:text-3xl">
+      <div className="text-xl md:text-3xl">
         Detected Emotion: <span className="text-yellow-400">{emotion}</span>
       </div>
     </div>
--- src/pages/Detect.jsx
+++ src/pages/Detect.jsx
@@ -18,7 +18,7 @@ const Detect = () => {
   }, []);
 
   return (
-    <div className="min-h-screen mt-10 md:mt-10 flex flex-col md:flex-row gap-30 md:gap-20 justify-center items-center p-10 overflow-hidden">
+    <div className="min-h-screen mt-10 md:mt-10 flex flex-col md:flex-row gap-40 sm:gap-50 md:gap-20 justify-center items-center p-10 overflow-hidden">
       {loading ? (
         <div className="flex justify-center items-center w-full">
           <BounceLoader color="#ffffff" />
